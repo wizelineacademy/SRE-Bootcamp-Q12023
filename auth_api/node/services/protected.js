80cbb86ee105c42e426cb0cc095a82d666b11fe2
Hernán Adalid <escalera_perez@ucol.mx>
2023-02-21 01:01:36 -0600

    cambios de archivos

--- auth_api/node/services/protected.js
+++ auth_api/node/services/protected.js
@@ -1,3 +1,27 @@
 export const protectFunction = (authorization) => {
-  return 'test';
+    let splitted=authorization.split(' ');
+    let token="";
+    switch (splitted.length) {
+        case 1:
+            token=splitted[0];
+            break;
+        case 2:
+            if (splitted[0] != 'Bearer')
+                 return "forbidden";
+            token=splitted[1];
+            break;
+        case 3:
+            if (!token) 
+              return "forbidden";
+        default:
+            return "forbidden";
+            break;
+    }
+    const jwt= require ('jsonwebtoken');
+    try {
+        let decoded =jwt.verify(token,'my2w7wjd7yXF64FIADfJxNs1oupTGAuW');
+        return "You are under protected data";
+    } catch (e) {
+        return e.message;
+    }
 }
